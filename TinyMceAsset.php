3c184c9f28dba64d8ffa7010f181d48d23e8e2f0
aasidorin <dumuro@gmail.com>
2017-12-14 17:22:25 +0300

    +

--- TinyMceAsset.php
+++ TinyMceAsset.php
@@ -9,7 +9,7 @@ namespace dumuro\tinymce;
 class TinyMceAsset extends \yii\web\AssetBundle
 {
 
-    public $sourcePath = '@bower-asset/tinymce';
+    public $sourcePath = '@bower/tinymce';
     public $js = [
         'tinymce.jquery.min.js',
         'jquery.tinymce.min.js'
